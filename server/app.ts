effa9089fcf4cba90658155250f265eaef2cf9ad
het <hetc1639@gmail.com>
2025-02-15 22:14:08 +0530

    commit 3

--- server/app.ts
+++ server/app.ts
@@ -39,7 +39,8 @@ const init = async () => {
         })
     );
 
-    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.oisbb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@clusterstock.mmios.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Clusterstock`;
+
 
     mongoose
         .connect(uri)
